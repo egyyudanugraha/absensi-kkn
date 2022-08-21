3196a159b504d385d0eb6ee06c2e7246e326cba2
EGYuda23 <egyyudanugraha@gmail.com>
2022-08-21 23:11:17 +0700

    add riwayat kas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+    <title>Login | KKN Suradita</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
