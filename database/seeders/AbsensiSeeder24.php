66759bcc1768079e1ec041a3feef8286642e507d
EGYuda23 <egyyudanugraha@gmail.com>
2022-08-25 18:01:00 +0700

    add seeder absensi backup 24 ags

---
+++ database/seeders/AbsensiSeeder24.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AbsensiSeeder24 extends Seeder
+{    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $absensi = [
+            [
+              "alasan" => NULL,
+              "anggota_id" => 8,
+              "created_at" => "2022-08-23 03:58:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:58:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 2,
+              "created_at" => "2022-08-23 03:58:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:58:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 2,
+              "created_at" => "2022-08-23 22:58:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 22:58:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 16,
+              "created_at" => "2022-08-23 02:58:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 02:58:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 13,
+              "created_at" => "2022-08-23 03:46:00",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:46:00"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 13,
+              "created_at" => "2022-08-24 03:46:00",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 03:46:00"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 10,
+              "created_at" => "2022-08-24 06:58:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 06:58:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 22,
+              "created_at" => "2022-08-23 03:47:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:47:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 22,
+              "created_at" => "2022-08-24 04:03:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 04:03:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 9,
+              "created_at" => "2022-08-24 03:46:48",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 03:46:48"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 9,
+              "created_at" => "2022-08-24 07:21:49",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 07:21:49"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 19,
+              "created_at" => "2022-08-23 03:49:49",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:49:49"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 19,
+              "created_at" => "2022-08-24 07:27:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 07:27:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 20,
+              "created_at" => "2022-08-23 03:49:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:49:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 11,
+              "created_at" => "2022-08-23 03:47:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:47:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 18,
+              "created_at" => "2022-08-23 03:46:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:46:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 17,
+              "created_at" => "2022-08-23 03:49:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:49:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 23,
+              "created_at" => "2022-08-24 05:49:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 05:49:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 21,
+              "created_at" => "2022-08-24 01:19:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 01:19:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 1,
+              "created_at" => "2022-08-23 21:26:07",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 21:26:07"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 8,
+              "created_at" => "2022-08-24 15:00:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:00:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 20,
+              "created_at" => "2022-08-24 15:00:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:00:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 11,
+              "created_at" => "2022-08-24 15:00:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:00:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 17,
+              "created_at" => "2022-08-24 15:00:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:00:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 23,
+              "created_at" => "2022-08-23 03:00:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 03:00:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 21,
+              "created_at" => "2022-08-23 02:15:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 02:15:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 1,
+              "created_at" => "2022-08-23 01:15:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-23 01:15:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 7,
+              "created_at" => "2022-08-24 15:15:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:15:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 6,
+              "created_at" => "2022-08-24 15:15:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:15:50"
+            ],
+            [
+              "alasan" => NULL,
+              "anggota_id" => 12,
+              "created_at" => "2022-08-24 15:15:50",
+              "status" => "hadir",
+              "updated_at" => "2022-08-24 15:15:50"
+            ]
+        ];
+
+        DB::table('absensis')->insert($absensi);
+    }
+}
